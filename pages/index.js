d743ac355cdfefad673990c2bb6e947ffc9cc715
Zenith29 <99365499+Zenith29@users.noreply.github.com>
2022-02-09 16:33:44 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,65 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+<!DOCTYPE html>
+<html>
+<head>
+      <link rel="stylesheet" type="text/css" href="./style/font.css">
+      <link rel="stylesheet" href="./style/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Schoolist3r">
+  <meta name="description" content="Schoolist3r creates the highest quality anti-censorship services, completely free of charge! We also have an extensive arcade and are community-driven.">
+<title>Schoolist3r</title>
+<link rel="icon" href="./unknown.png ">
+</head>
+      <style>
+body {
+  zoom: 100%;
 }
+
+@media only screen and (max-width: 1024px) {
+  body {
+    zoom: 70%;
+  }
+}
+</style>
+<body style="background-color:#001260;color:#FFF;">
+  <ul class="topnav">
+  <li style="font-family:comicsansms;display:block"><a href="Index.html">Schoolist3r</a></li>
+  <li class="right"><a href="About.html">About</a></li>
+  <li class="right"><a href="Tools.html">Useful Tools</a></li>
+  <li class="right"><a href="Sel.html">Surf freely!</a></li>
+  <li class="right"><a href="Games.html">Arcade Games</a></li>
+</ul>
+<center>
+<h1 style="font-family:comicsansms;color:white;"><ins>Schoolist3r</ins></h1>
+<img src="download.gif" width= 300px height= 151px alt= gif />
+<p style=";color:white;">Note: Can't Stop | Stay Safe during the COVID-19 Pandemic!</p>
+<h2 style="font-family:Agency FB; font-size:50px;color:white;">Made for students, by students</h2>
+<br>
+<iframe data-aa="1392267" src="//ad.a-ads.com/1392267?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
+<br>
+<br>
+<p style="Calibri;color:white;">
+<b><i><u>
+  We are currently working to patch bugs. Last Updated 11/25/20 | <a href="changelog.txt">View Changelog</a> </u></i></b>
+</p>
+</style>
+</br>
+</br>
+<a href="Games.html"><button class="selbutton"><ins>Arcade</ins></button></a>
+<a href="Sel.html"><button class="selbutton"><ins>Surf Freely</ins></button></a>
+  <a href="Tools.html"><button class="selbutton"><ins>Useful Tools</ins></button></a>
+  <a href="Bookmarklets.html"><button class="selbutton"><ins>Bookmarklets</ins></button></a>
+<br>
+</br>
+<br>
+<script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"></script>
+</body>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165254470-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-165254470-1');
+</script>
+  <p><a href="Tos.html"> TOS</a>   |   <a href="Pp.html"> Privacy Policy</a></p>
